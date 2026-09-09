362ffdb93aae32c1c30399a5c040301f82dc9055
MauroPX <34166897+MauroPX@users.noreply.github.com>
2026-09-09 15:38:16 -0700

    Revert "feat(excalibur): galería de roles de color M3 en vivo en el meta-caso"
    This reverts commit c0cc2ac82fcc0ae31df3714bee3f10bdc843d0ad.

--- src/app/[locale]/excalibur/page.tsx
+++ src/app/[locale]/excalibur/page.tsx
@@ -4,7 +4,6 @@ import { CasePage } from '@/components/templates/CasePage'
 import { CreativeWorkJsonLd } from '@/components/infra/JsonLd'
 import { getMetaCase } from '@/content/cases'
 import type { AppLocale } from '@/i18n/routing'
-import { ColorRolesShowcase } from './ColorRolesShowcase'
 
 export async function generateMetadata({
   params,
@@ -36,7 +35,7 @@ export default async function ExcaliburMetaCase({
   return (
     <>
       <CreativeWorkJsonLd name={meta.title} description={meta.description} url="/excalibur" />
-      <CasePage caseData={meta} appendixSection={<ColorRolesShowcase />} />
+      <CasePage caseData={meta} />
     </>
   )
 }
